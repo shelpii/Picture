b5478b9b63e2ddcde68edf1912525ac27178d42b
shelpii <umbrasasdima@gmail.com>
2024-11-07 15:46:09 -0600

    Created a mask for the phone number

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import modals from "./modules/modals";
 import sliders from "./modules/sliders";
 import forms from "./modules/forms";
+import mask from "./modules/mask";
+import checkTextInputs from "./modules/checkTextInputs";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use-strict';
@@ -9,5 +11,9 @@ window.addEventListener('DOMContentLoaded', () => {
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn', { enabled: true, speed: 4000 }, true);
     sliders('.main-slider-item', 'vertical', '', '', { enabled: true, speed: 4000 }, true);
     forms();
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
+
 
 });
