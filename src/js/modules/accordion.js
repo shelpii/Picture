0d93fc340014adc1fc762b926e3b34ee03db3a3f
shelpii <umbrasasdima@gmail.com>
2024-11-12 19:31:14 -0600

    modification of the module accordion.js

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,14 +1,21 @@
-
-
 const accordion = (triggersSelector) => {
     const btns = document.querySelectorAll(triggersSelector);
 
     btns.forEach(btn => {
         btn.addEventListener('click', function() {
+            // Закрываем все аккордеоны перед открытием текущего
+            btns.forEach(otherBtn => {
+                if (otherBtn !== this) {
+                    otherBtn.classList.remove('active-style');
+                    otherBtn.nextElementSibling.classList.remove('active-content');
+                    otherBtn.nextElementSibling.style.maxHeight = '0px';
+                }
+            });
+            
             this.classList.toggle('active-style');
             this.nextElementSibling.classList.toggle('active-content');
 
-            if(this.classList.contains('active-style')) {
+            if (this.classList.contains('active-style')) {
                 this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + "px";
             } else {
                 this.nextElementSibling.style.maxHeight = '0px';
